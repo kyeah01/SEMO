3637a2e66a13105866f5bbe18bc2264c6b66d3c3
김구현 <siva3020@naver.com>
2019-10-17 14:10:58 +0900

    191017 || Font-awesome && Font Defalut Setting

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -40,13 +40,5 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Noto+Sans+KR:400,500,700,900|Noto+Sans:400,700,700i&display=swap&subset=korean');
-#app {
-  font-family: 'Noto Sans', 'Noto Sans KR','Avenir', Helvetica, Arial, sans-serif ;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
