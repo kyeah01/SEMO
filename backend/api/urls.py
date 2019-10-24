ff455b31259f6df5afccf250f91c33c0764b9e4a
전영진 <arian2483@naver.com>
2019-10-24 09:55:57 +0900

    191024 | auth && knox merge

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,11 @@
-from django.contrib import admin
 from django.urls import path
 from .views.views import *
+from api.views.auth_views import RegistrationAPI, LoginAPI, UserAPI
 
 urlpatterns = [
+    path("auth/register/", RegistrationAPI.as_view()),
+    path("auth/login/", LoginAPI.as_view()),
+    path("auth/user/", UserAPI.as_view()),
     path('site/list/', APIListView.as_view()),
     path('site/detail/<int:obj_pk>', APIDetailView.as_view()),
 ]
