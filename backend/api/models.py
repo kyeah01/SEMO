d6563815cec1654e9ed337125b524aa3b3723949
kyeah01 <kyeah01@gmail.com>
2019-10-29 17:56:47 +0900

    191029 | feat: DB 구조변경 - end points model add - api list model modify

--- backend/api/models.py
+++ backend/api/models.py
@@ -9,14 +9,18 @@ User = get_user_model()
 # 2019.10.22 PM 2:23
 # 보여줄 확정 정보 / request를 받을 정보를 담을 model의 구조가 비슷하므로
 # 이를 상속시켜줄 model APIList 구현.
+class Category(models.Model):
+    name = models.CharField(max_length=140)
+
 class APIList(models.Model):
     title = models.TextField()
     url = models.TextField()
-    category = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.TextField()
     howToUse = models.TextField() 
     maxRequestCount = models.TextField()
     expiredDate = models.TextField()
+    needAuthentication = models.BooleanField()
     
     class Meta:
         abstract = True
@@ -29,6 +33,13 @@ class EditedList(APIList):
     requestUser = models.ManyToManyField(User)
     check = models.BooleanField(default=False)
     accept = models.BooleanField(default=False)
+
+class EndPoints(models.Model):
+    apisite = models.ForeignKey(APISite, on_delete=models.CASCADE)
+    isAuthentication = models.BooleanField()
+    pathParams = models.TextField()
+    queryString = models.TextField()
+    response = models.TextField()
 
 class RegisterList(APIList):
     requestUser = models.ManyToManyField(User)
